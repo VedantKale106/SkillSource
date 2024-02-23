784418a6857ca0d82d64d0b90abfc49a8c42c0d5
VedantKale106 <vedanthkale777@gmail.com>
2024-02-23 22:06:21 +0530

    Login Page Draft 1

---
+++ app/src/main/java/com/vedant/skillsource/Login.java
@@ -0,0 +1,29 @@
+package com.vedant.skillsource;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    Button signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        signup = findViewById(R.id.signup);
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Register.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
